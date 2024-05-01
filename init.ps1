0e1641b67a4d5aea18591304ec030c3ced2c4174
Santosh Das <santosh.cloud003@gmail.com>
2024-05-01 12:12:52 +0530

    demo setup

--- init.ps1
+++ init.ps1
@@ -1,4 +1,4 @@
-$prefix = "alef"
+$prefix = "demo"
 az group create -l WestEurope -n "tfstate-$($prefix)-rg"
 az storage account create -n "tfstatestore$($prefix)" -g "tfstate-$($prefix)-rg" -l WestEurope --sku Standard_LRS
 az storage container create -n "tf-state" --account-name "tfstatestore$($prefix)" --public-access container
